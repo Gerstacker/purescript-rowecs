f7c0ba153e78a6ee49eca4b405312bd2926a6003
Gerstacker <alex.schndr@gmail.com>
2017-12-21 17:32:54 -0800

    rename some type variables

--- src/ECS.purs
+++ src/ECS.purs
@@ -2,17 +2,16 @@ module ECS where
 
 import Data.Maybe
 
-import Control.Monad.Rec.Class (tailRec)
 import Data.IntMap as IM
 import Data.Record (insert, get, set) as R
 import Type.Prelude (class IsSymbol, class RowLacks, class RowToList, RLProxy(RLProxy), SProxy(SProxy), RProxy)
 import Type.Proxy (Proxy2(Proxy2))
 import Type.Row (Cons, Nil, kind RowList)
 
-class Storage (m :: Type -> Type) a where
-  allocate :: m a
-  get :: m a -> Int -> Maybe a
-  set :: m a -> Int -> a -> m a
+class Storage (c :: Type -> Type) a where
+  allocate :: c a
+  get :: c a -> Int -> Maybe a
+  set :: c a -> Int -> a -> c a
 
 
 instance storageIntMap :: Storage IM.IntMap a where
@@ -22,97 +21,61 @@ instance storageIntMap :: Storage IM.IntMap a where
 
 
 
-type ECS rowst (m :: Type -> Type) = (Record (storage :: (Record rowst), nextID :: Int))
+type ECS (c :: Type -> Type) rowS = (Record (storage :: (Record rowS), nextID :: Int))
 
 
-read :: forall rowst name a m tail
-  . Storage m a
+read :: forall rowS name a c rowS'
+  . Storage c a
   => IsSymbol name
-  => RowCons name (m a) tail rowst
-  => ECS rowst m -> SProxy name -> Int -> Maybe a
+  => RowCons name (c a) rowS' rowS
+  => ECS c rowS -> SProxy name -> Int -> Maybe a
 read ecs spr ind = get v ind
   where
-    v = (R.get spr ecs.storage) :: m a
+    v = (R.get spr ecs.storage) :: c a
 
-write :: forall rowst name a m tail
-  . Storage m a
+write :: forall rowS name a c rowS'
+  . Storage c a
   => IsSymbol name
-  => RowCons name (m a) tail rowst
-  => ECS rowst m -> SProxy name -> Int -> a -> ECS rowst m
+  => RowCons name (c a) rowS' rowS
+  => ECS c rowS -> SProxy name -> Int -> a -> ECS c rowS
 write ecs spr ind val = ecs { storage = stor' }
   where
-    intmap = (R.get spr ecs.storage) :: m a
+    intmap = (R.get spr ecs.storage) :: c a
     intmap' = set intmap ind val
     stor' = R.set spr intmap' ecs.storage
 
 
 
-class AllocateStorage (xs :: RowList) (row :: # Type) a (row' :: # Type) (m :: Type -> Type)
-    | xs m -> row row' a, row' -> m row
+class AllocateStorage (c :: Type -> Type) (rowDg :: # Type) (listD :: RowList)  (rowS :: # Type) a
+    | listD c -> rowDg rowS a, rowS -> c rowDg
   where
-    allocateStorageImpl :: RLProxy xs -> Proxy2 m -> ECS row' m
+    allocateStorageImpl :: RLProxy listD -> Proxy2 c -> ECS c rowS
 
-instance allocateStorageNil :: AllocateStorage Nil row a () m where
+instance allocateStorageNil :: AllocateStorage m rowDg Nil () a where
   allocateStorageImpl _ _ = { storage : {}, nextID : 0}
 
 instance allocateStorageCons ::
   ( IsSymbol name
-  , RowCons name a trash row
-  , AllocateStorage tail row b tailRow' m
-  , RowLacks name tailRow'
-  , RowCons name (m a) tailRow' row'
-  , Storage m a
-  ) => AllocateStorage (Cons name a tail) row a row' m where
+  , Storage c a
+  , RowCons name (c a) rowS' rowS
+  , RowLacks name rowS'
+  , AllocateStorage c rowD listD' rowS' b
+  ) => AllocateStorage c rowDg (Cons name a listD') rowS a where
   allocateStorageImpl _ _ =
     (rest { storage = stor})
     where
       stor = R.insert nameP val rest.storage
       nameP = SProxy :: SProxy name
       val = allocate
-      rest = allocateStorageImpl (RLProxy :: RLProxy tail) (Proxy2 :: Proxy2 m) :: ECS tailRow' m
+      rest = allocateStorageImpl (RLProxy :: RLProxy listD') (Proxy2 :: Proxy2 c) :: ECS c rowS'
 
-allocateStorage :: forall m row xs a row'
-  . RowToList row xs
-  => AllocateStorage xs row a row' m
-  => Storage m a
-  => RProxy row
-  -> Proxy2 m
-  -> ECS row' m
-allocateStorage _ = allocateStorageImpl (RLProxy :: RLProxy xs)
+allocateStorage :: forall c rowD listD a rowS
+  . RowToList rowD listD
+  => AllocateStorage c rowD listD rowS a
+  => Storage c a
+  => RProxy rowD
+  -> Proxy2 c
+  -> ECS c rowS
+allocateStorage _ = allocateStorageImpl (RLProxy :: RLProxy listD)
 
-{-
-
-class ReadStorage (srow :: # Type) (xs :: RowList) (row :: # Type) a (row' :: # Type) (m :: Type -> Type)
-    | xs m -> row row' a, row' -> m row
-  where
-    readStorageImpl :: RLProxy xs -> ECS row' m -> Int -> Record row
-
-instance readStorageNil :: ReadStorage srow Nil row a () m where
-  readStorageImpl _ _ _= {}
-
-instance readStorageCons ::
-  ( IsSymbol name
-  , RowCons name a trash row
-  , ReadStorage srow tail row b tailRow' m
-  , RowLacks name tailRow'
-  , RowCons name (m a) tailRow' row'
-  , Storage m a
-  ) => ReadStorage srow (Cons name a tail) row a row' m where
-  readStorageImpl _ _ =
-    (rest { storage = stor})
-    where
-      stor = R.insert nameP val rest.storage
-      nameP = SProxy :: SProxy name
-\      val = read
-      rest = readStorageImpl (RLProxy :: RLProxy tail) (Proxy2 :: Proxy2 m) :: ECS tailRow' m
-
-
-readStorage :: forall m srow row xs a row'
-  . RowToList row xs
-  => ReadStorage srow xs row a row' m
-  => Storage m a
-  => ECS srow
-  -> Int
-  -> Record row
-readStorage = readStorageImpl (RLProxy :: RLProxy xs)
--}
+--class ReadStorage (c :: Type -> Type) (rowSg :: # Type) (rowDg :: # Type) (listD :: RowList) (rowS :: # Type) a
